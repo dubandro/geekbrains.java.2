918a71daf1f39be82ea1904595d5f2bc5a80587d
dubandro <dubovik@internet.ru>
2021-02-26 18:51:43 +0200

    home work - lesson 2

--- src/geekbrains/dubovik/java/lesson_2/lesson2.java
+++ src/geekbrains/dubovik/java/lesson_2/lesson2.java
@@ -1,4 +1,11 @@
 package geekbrains.dubovik.java.lesson_2;
 
 public class lesson2 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("lesson 2 - это заглушка, сегодня-завтра сделаю");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
