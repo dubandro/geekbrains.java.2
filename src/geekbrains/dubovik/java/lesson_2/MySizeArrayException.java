1ae56e8c8a9ed53d0e0c3397fc528aea0cc3ca3d
dubandro <dubovik@internet.ru>
2021-03-01 19:18:01 +0200

    home work - lesson 2

---
+++ src/geekbrains/dubovik/java/lesson_2/MySizeArrayException.java
@@ -0,0 +1,7 @@
+package geekbrains.dubovik.java.lesson_2;
+
+public class MySizeArrayException extends RuntimeException{
+    MySizeArrayException (String message) {
+        super(message);
+    }
+}
--- src/geekbrains/dubovik/java/lesson_2/lesson2.java
+++ src/geekbrains/dubovik/java/lesson_2/lesson2.java
@@ -1,11 +1,47 @@
 package geekbrains.dubovik.java.lesson_2;
 
+import java.util.Arrays;
+
 public class lesson2 {
     public static void main(String[] args) {
-        try {
-            System.out.println("lesson 2 - это заглушка, сегодня-завтра сделаю");
-        } catch (Exception e) {
-            e.printStackTrace();
+        final int SIZE = 4;
+
+        String[][] array1 = new String[][] {
+                {"11", "12", "13", "14"},
+                {"21", "22", "23", "24"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"},
+                {"51"}
+        };
+
+        String[][] array2 = new String[][] {
+                {"11", "12", "13", "14", "15"},
+                {"21", "22", "23", "24"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}
+        };
+
+        String[][] array3 = new String[][] {
+                {"11", "12", "13", "14"},
+                {"21", "22", "23", "24"},
+                {"31", "32", "ЗЗ", "34"},
+                {"41", "42", "43", "44"}
+        };
+
+        String[][] array4 = new String[][] {
+                {"11", "12", "13", "14"},
+                {"21", "22", "23", "24"},
+                {"31", "32", "33", "34"},
+                {"41", "42", "43", "44"}
+        };
+
+        for (String[][] arrays : Arrays.asList(array1, array2, array3, array4)) {
+            try {
+                CheckArray.chekAndSumArray(arrays, SIZE);
+            }
+            catch (Exception exArr) {
+                System.out.println(exArr.getMessage());
+            }
         }
     }
 }
