b88170af20156f0fb24e328dfae759f33b6fe11e
dubandro <dubovik@internet.ru>
2021-03-10 11:00:57 +0200

    home work - lesson 5 - вставил потоки

--- src/geekbrains/dubovik/java/lesson_5/ArrayThread.java
+++ src/geekbrains/dubovik/java/lesson_5/ArrayThread.java
@@ -3,47 +3,48 @@ package geekbrains.dubovik.java.lesson_5;
 public class ArrayThread {
 
     public static void threadOne(float[] arr) {
-        for (float i : arr) System.out.print(i);
-        System.out.println();
 
         long start = System.currentTimeMillis();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        long finish = System.currentTimeMillis();
-        System.out.printf("Изменение целого массива произведено за %d секунд\n", finish - start);
+        int range = 0;
+        Thread t = new NewThread(arr, range);
+        t.start();
 
-        for (float i : arr) System.out.print(i);
-        System.out.println();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long finish = System.currentTimeMillis();
+        System.out.printf("Изменение целого массива произведено за %d милисекунд\n", finish - start);
     }
 
     public static void threadTwo(float[] arr) {
-        for (float i : arr) System.out.print(i);
-        System.out.println();
 
         long start = System.currentTimeMillis();
 
-        int h = arr.length / 2;
-        float[] arr21 = new float[h];
-        float[] arr22 = new float[h];
-        System.arraycopy(arr, 0, arr21, 0, h);
-        System.arraycopy(arr, h, arr22, 0, h);
+        int range = arr.length / 2;
+        float[] arr21 = new float[range];
+        float[] arr22 = new float[range];
+        System.arraycopy(arr, 0, arr21, 0, range);
+        System.arraycopy(arr, range, arr22, 0, range);
 
-        for (int i = 0; i < arr21.length; i++) {
-            arr21[i] = (float)(arr21[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        for (int i = 0; i < arr22.length; i++) {
-            int j = i + h;
-            arr22[i] = (float)(arr22[i] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
+        Thread t1 = new NewThread(arr21, 0);
+        Thread t2 = new NewThread(arr22, range);
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        System.arraycopy(arr21, 0, arr, 0, h);
-        System.arraycopy(arr22, 0, arr, h, h);
+        System.arraycopy(arr21, 0, arr, 0, range);
+        System.arraycopy(arr22, 0, arr, range, range);
 
         long finish = System.currentTimeMillis();
-        System.out.printf("Разделение на два массива, изменение и склейка произведено за %d секунд\n", finish - start);
-
-        for (float i : arr) System.out.print(i);
-        System.out.println();
+        System.out.printf("Разделение на два массива, изменение и склейка произведено за %d милисекунд\n", finish - start);
     }
 }
